cffc441a934622f05317e9f015342709eb0e7611
worashf <uppert83@gmail.com>
2022-10-18 02:34:04 +0300

    Add styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1 style={{ color: 'red' }}>Hello World!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
